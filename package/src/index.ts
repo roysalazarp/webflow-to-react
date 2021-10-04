12d222f5825f85627f948383d50728729dde5dc0
Roy Salazar <roysalazarp@gmail.com>
2021-10-04 18:27:34 +0300

    removed inquirer

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import inquirer from "inquirer";
+// import inquirer from "inquirer";
+import findConfig from "find-config";
 import * as fs from "fs";
 import cheerio from "cheerio";
 // import { dirname } from "path";
@@ -9,65 +10,100 @@ const CURR_DIR = process.cwd();
 
 // const __dirname = dirname(fileURLToPath(import.meta.url));
 
-interface IQUESTIONS {
-  [k: string]: any;
+// interface IQUESTIONS {
+//   [k: string]: any;
+// }
+
+// const QUESTIONS: IQUESTIONS[] = [
+//   {
+//     name: "webflow-folder-path",
+//     type: "input",
+//     message: "where is your webflow folder located:",
+//     validate: function (input: string) {
+//       if (/^([A-Za-z\-\/..\_\d])+$/.test(input)) return true;
+//       else
+//         return "Project name may only include letters, numbers, underscores and hashes.";
+//     },
+//   },
+//   {
+//     name: "select-component-from-html",
+//     type: "input",
+//     message: "given name to component from html to convert:",
+//     validate: function (input: string) {
+//       if (/^([A-Za-z\-\\_\d])+$/.test(input)) return true;
+//       else
+//         return "Project name may only include letters, numbers, underscores and hashes.";
+//     },
+//   },
+//   {
+//     name: "component-interface",
+//     type: "input",
+//     message: "Point the file where is the interface for your component:",
+//     validate: function (input: string) {
+//       if (/^([A-Za-z\-\/..\_\d])+$/.test(input)) return true;
+//       else
+//         return "Project name may only include letters, numbers, underscores and hashes.";
+//     },
+//   },
+// ];
+
+// inquirer.prompt(QUESTIONS).then((answers) => {
+//   const divNameInHtmlToConvertToReactComponent =
+//     answers["select-component-from-html"];
+
+//   const webflowFolderPath = readConfigFile();
+
+//   const givenComponentInterface = answers["component-interface"];
+//   const componentInterface = `${CURR_DIR}/${givenComponentInterface}`;
+
+//   const webflowHtml = fs.readFileSync(webflowFolderPath, "utf8");
+//   const interfaceForTheNewReactComponentToBeCreated = fs.readFileSync(
+//     componentInterface,
+//     "utf8"
+//   );
+
+//   transformComponent(
+//     divNameInHtmlToConvertToReactComponent,
+//     interfaceForTheNewReactComponentToBeCreated,
+//     webflowHtml
+//   );
+// });
+
+interface Config {
+  webflowFolder: string;
 }
 
-const QUESTIONS: IQUESTIONS[] = [
-  {
-    name: "webflow-folder-path",
-    type: "input",
-    message: "where is your webflow folder located:",
-    validate: function (input: string) {
-      if (/^([A-Za-z\-\/..\_\d])+$/.test(input)) return true;
-      else
-        return "Project name may only include letters, numbers, underscores and hashes.";
-    },
-  },
-  {
-    name: "select-component-from-html",
-    type: "input",
-    message: "given name to component from html to convert:",
-    validate: function (input: string) {
-      if (/^([A-Za-z\-\\_\d])+$/.test(input)) return true;
-      else
-        return "Project name may only include letters, numbers, underscores and hashes.";
-    },
-  },
-  {
-    name: "component-interface",
-    type: "input",
-    message: "Point the file where is the interface for your component:",
-    validate: function (input: string) {
-      if (/^([A-Za-z\-\/..\_\d])+$/.test(input)) return true;
-      else
-        return "Project name may only include letters, numbers, underscores and hashes.";
-    },
-  },
-];
+const projectRootDirPath = findConfig("wtr.config.json")!.replace(
+  /\/wtr.config.json/g,
+  ""
+);
 
-inquirer.prompt(QUESTIONS).then((answers) => {
-  const divNameInHtmlToConvertToReactComponent =
-    answers["select-component-from-html"];
+console.log(projectRootDirPath);
 
-  const givenWebflowFolderPath = answers["webflow-folder-path"];
-  const webflowFolderPath = `${CURR_DIR}/${givenWebflowFolderPath}webflow/index.html`;
-
-  const givenComponentInterface = answers["component-interface"];
-  const componentInterface = `${CURR_DIR}/${givenComponentInterface}`;
-
-  const webflowHtml = fs.readFileSync(webflowFolderPath, "utf8");
-  const interfaceForTheNewReactComponentToBeCreated = fs.readFileSync(
-    componentInterface,
+const readConfigFile = (): Config => {
+  const configFileString = fs.readFileSync(
+    `${projectRootDirPath}/wtr.config.json`,
     "utf8"
   );
+  const configFileJson: Config = JSON.parse(configFileString);
+  const output = configFileJson;
+  return output;
+};
 
-  transformComponent(
-    divNameInHtmlToConvertToReactComponent,
-    interfaceForTheNewReactComponentToBeCreated,
-    webflowHtml
-  );
-});
+const componentName = /(?!(.*\/))\w+/g.exec(CURR_DIR)![0];
+
+const divNameInHtmlToConvertToReactComponent = componentName;
+
+const webflowHtml = (() => {
+  const webflowFolderLocation = readConfigFile().webflowFolder;
+  const locationOfWebflowPath = `${projectRootDirPath}/${webflowFolderLocation}/webflow/index.html`;
+  return fs.readFileSync(locationOfWebflowPath, "utf8");
+})();
+
+const interfaceForTheNewReactComponentToBeCreated = (() => {
+  const interfacePath = `${CURR_DIR}/${componentName}.interface.ts`;
+  return fs.readFileSync(interfacePath, "utf8");
+})();
 
 const transformComponent = (
   divNameInHtmlToConvertToReactComponent: string,
@@ -128,7 +164,7 @@ const transformComponent = (
   const nameOfNewReactComponent = divNameInHtmlToConvertToReactComponent;
   const nameOfNewReactComponentController = `${nameOfNewReactComponent}Controller`;
 
-  const folderWhereNewReactComponentWillBePlaced = `${CURR_DIR}/${divNameInHtmlToConvertToReactComponent}`;
+  const folderWhereNewReactComponentWillBePlaced = `${CURR_DIR}`;
   const reactComponentFile = `${folderWhereNewReactComponentWillBePlaced}/${nameOfNewReactComponent}.tsx`;
   const reactComponentControllerFile = `${folderWhereNewReactComponentWillBePlaced}/index.tsx`;
 
@@ -143,10 +179,6 @@ const transformComponent = (
   const componentPropsString = componentPropsArray
     .toString()
     .replace(/,/gm, " ");
-
-  if (!fs.existsSync(folderWhereNewReactComponentWillBePlaced)) {
-    fs.mkdirSync(folderWhereNewReactComponentWillBePlaced);
-  }
 
   fs.writeFileSync(
     reactComponentFile,
@@ -178,9 +210,9 @@ const createReactComponent = (
 // THIS IS A GENERATED FILE, do not modify.
 
 import { FunctionComponent } from 'react';
-import IProps from '../interfaces/${nameOfNewReactComponent}';
+import I${nameOfNewReactComponent} from './${nameOfNewReactComponent}.interface';
 
-const ${nameOfNewReactComponent}: FunctionComponent<IProps> = (props) => {
+const ${nameOfNewReactComponent}: FunctionComponent<I${nameOfNewReactComponent}> = (props) => {
   const {${props}} = props;
   const {children} = props;
 
@@ -201,9 +233,9 @@ const createReactComponentController = (
   props: string[]
 ) => `
 import ${nameOfNewReactComponent} from "./${nameOfNewReactComponent}";
-import IProps from "../interfaces/${nameOfNewReactComponent}";
+import I${nameOfNewReactComponent} from "./${nameOfNewReactComponent}.interface";
 
-const ${nameOfNewReactComponentController} = (props: IProps) => {
+const ${nameOfNewReactComponentController} = (props: I${nameOfNewReactComponent}) => {
   const {${props}} = props;
 
   return <${nameOfNewReactComponent} ${componentPropsString} />;
@@ -211,3 +243,9 @@ const ${nameOfNewReactComponentController} = (props: IProps) => {
 
 export default ${nameOfNewReactComponentController};
 `;
+
+transformComponent(
+  divNameInHtmlToConvertToReactComponent,
+  interfaceForTheNewReactComponentToBeCreated,
+  webflowHtml
+);
